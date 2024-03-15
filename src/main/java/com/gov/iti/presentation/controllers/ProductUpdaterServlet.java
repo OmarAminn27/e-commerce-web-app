07456be1bd9a71757338c9edb520d5cbcca26d43
omar <omaraminn27@gmail.com>
2024-03-15 19:41:09 +0200

    add feature admin displaying products and editing them

---
+++ src/main/java/com/gov/iti/presentation/controllers/ProductUpdaterServlet.java
@@ -0,0 +1,47 @@
+package com.gov.iti.presentation.controllers;
+
+import com.google.gson.Gson;
+import com.gov.iti.business.dtos.ProductDTO;
+import com.gov.iti.business.entities.Product;
+import com.gov.iti.business.services.ProductUpdaterService;
+import com.gov.iti.business.services.ProductsDisplayerService;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+
+public class ProductUpdaterServlet extends HttpServlet {
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        BufferedReader reader = req.getReader();
+        StringBuilder jsonBody = new StringBuilder();
+        String line;
+        while ((line = reader.readLine()) != null) {
+            jsonBody.append(line);
+        }
+
+        ProductDTO productDTO = parseJsonToProduct(jsonBody.toString());
+        System.out.println("Received productDTO: " + productDTO.toString());
+        EntityManagerFactory emf = (EntityManagerFactory) req.getServletContext().getAttribute("emf");
+        ProductUpdaterService productUpdaterService = new ProductUpdaterService(emf);
+
+        Product productFromDB = productUpdaterService.findProductByName(productDTO.getProductName());
+        productFromDB.setDescription(productDTO.getDescription());
+        productFromDB.setQuantity(productDTO.getQuantity());
+        productFromDB.setPrice(productDTO.getPrice());
+        productFromDB.setCategory(productDTO.getCategory());
+
+        productUpdaterService.updateProduct(productFromDB);
+
+    }
+
+    private ProductDTO parseJsonToProduct(String json) {
+        Gson gson = new Gson();
+        return gson.fromJson(json, ProductDTO.class);
+    }
+}
